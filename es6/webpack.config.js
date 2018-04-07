ebe80976ddc74419429d496ccc5940ac9f151425
TODOR-PC\Todor <todor_1996@live.com>
2018-04-07 18:17:07 +0200

    Inicijalizacija projekta i instaliranje dependencyja

---
+++ es6/webpack.config.js
@@ -0,0 +1,34 @@
+const path=require('path');
+module.exports ={
+entry:'./src/index.js',
+watchOptions: {
+  poll: true
+},
+output:{
+    filename: "bundle.js",
+    path: path.resolve(__dirname,'dist'),
+    publicPath: "./dist/",
+},
+devtool: 'source-map',
+devServer: {
+  contentBase: __dirname,
+  inline: true,
+  host: '0.0.0.0',
+  port: 8080,
+  hot: true
+},
+module: {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2016']
+        }
+      }
+    }
+  ]
+}
+}
