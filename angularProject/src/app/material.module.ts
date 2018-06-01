a32d998258720970ae88047a23567500063e4820
c <cvoja99@hotmail.com>
2018-06-01 13:31:31 +0200

    Zavrsen dizajn za signUp form.

--- angularProject/src/app/material.module.ts
+++ angularProject/src/app/material.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from "@angular/core";
-import {MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule} from "@angular/material"
+import {MatButtonModule,MatNativeDateModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatCheckboxModule} from "@angular/material"
 @NgModule({
-    imports:[MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule],
-    exports:[MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule]
+    imports:[
+        MatButtonModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+    MatNativeDateModule,
+MatCheckboxModule],
+    exports:[
+        MatButtonModule,
+        MatNativeDateModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatCheckboxModule]
 })
 export class MaterialModule{}
